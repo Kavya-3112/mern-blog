800ccbc46bd9e48c5f8415d0d41c173cd919b154
kaviya <kaviyakalimuthu9865@gmail.com>
2024-07-03 12:09:09 +0530

    create user model

--- api/index.js
+++ api/index.js
@@ -1,5 +1,23 @@
-import express from "express";
+import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+import userRoutes from ''
+
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log('MongoDB is connected');
+  })
+  .catch((err) => {
+    console.log('MongoDB connection error:', err);
+  });
+
 const app = express();
 app.listen(3000, () => {
-    console.log('Server is running on port 3000');
+  console.log('Server is running on port 3000');
+});
+app.get('/test',(req,res)=>{
+    res.json({message:'ApI is working'})
 });
