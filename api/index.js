2ae7248117f5c91e538709ca77078c5f8f868dbb
kaviya <kaviyakalimuthu9865@gmail.com>
2024-07-03 15:10:33 +0530

    create and test api route

--- api/index.js
+++ api/index.js
@@ -1,23 +1,29 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
-import userRoutes from ''
+import userRoutes from './routes/user.route.js';
+import authRoutes from './routes/auth.route.js';
 
 dotenv.config();
 
+const app = express(); // Create an instance of the Express application
+
+app.use(express.json()); // Middleware to parse JSON bodies
+
 mongoose
-  .connect(process.env.MONGO)
+  .connect(process.env.MONGO, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => {
     console.log('MongoDB is connected');
   })
   .catch((err) => {
-    console.log('MongoDB connection error:', err);
+    console.log(err);
   });
 
-const app = express();
-app.listen(3000, () => {
-  console.log('Server is running on port 3000');
-});
-app.get('/test',(req,res)=>{
-    res.json({message:'ApI is working'})
+app.use('/api/user', userRoutes); // Use the user routes
+app.use('/api/auth', authRoutes); // Use the auth routes
+
+const PORT = process.env.PORT || 3000; // Define the port
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}!`);
 });
